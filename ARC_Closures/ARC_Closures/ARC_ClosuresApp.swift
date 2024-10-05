826e1894411c6358fd727801a7e6cb6281c34d20
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-05 17:13:02 +0200

    ARC with closures

---
+++ ARC_Closures/ARC_Closures/ARC_ClosuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARC_ClosuresApp.swift
+//  ARC_Closures
+//
+//  Created by Nivedha Rajendran on 05.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ARC_ClosuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
